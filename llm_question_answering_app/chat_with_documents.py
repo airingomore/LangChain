081a4896eb0144ddf22a75b3b4aca61a5afe9812
airingomore <airingomore@gmail.com>
2023-11-27 11:28:11 +0100

    added chat history

--- llm_question_answering_app/chat_with_documents.py
+++ llm_question_answering_app/chat_with_documents.py
@@ -64,6 +64,11 @@ def calculate_embedding_cost(texts):
     # print(f'Embeeding cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
     return total_tokens, total_tokens / 1000 * 0.0004
 
+
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
 if __name__ == "__main__":
     import os
     from dotenv import load_dotenv, find_dotenv
@@ -77,9 +82,9 @@ if __name__ == "__main__":
             os.environ['OPENAI_API_KEY']=api_key
 
         uploaded_file = st.file_uploader('Upload a file:', type=['pdf','docx','txt'])
-        chunk_size = st.number_input('Chunk size: ', min_value=100, max_value=2048, value=512)
-        k = st.number_input('k', min_value=1, max_value=20, value=3)
-        add_data = st.button('Add Data')
+        chunk_size = st.number_input('Chunk size: ', min_value=100, max_value=2048, value=512, on_change=clear_history)
+        k = st.number_input('k', min_value=1, max_value=20, value=3, on_change=clear_history)
+        add_data = st.button('Add Data', on_click=clear_history)
 
         if uploaded_file and add_data:
             
@@ -115,3 +120,11 @@ if __name__ == "__main__":
             st.write(f'k: {k}')
             answer = ask_and_get_answer(vector_store, q, k)
             st.text_area('LLM Answer: ', value=answer)
+
+            st.divider()
+            if 'history' not in st.session_state:
+                st.session_state.history = ''
+            value = f'Q: {q} \nA: {answer}'
+            st.session_state.history = f'{value} \n {"-"*100} \n {st.session_state.history}'
+            h = st.session_state.history
+            st.text_area(label='Chat History', value=h, key='history', height=400)
